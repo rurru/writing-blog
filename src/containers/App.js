0b6dea44cfecacd75a76d028e0afc3be48edc42f
rurru <rpreibisius@mdstaff.com>
2020-09-13 13:39:40 -0700

    Move app files to where they're supposed to go

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from '../components/Login';
+import Dashboard from '../components/Dashboard';
+import About from '../components/About';
+import withAuthentication from '../containers/withAuthentication';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/dashboard" component={withAuthentication(Dashboard)} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
