f86cf01cddbae153a9e75cce209a1c8c01cb8ee6
Robin Preibisius <rpreibisius@gmail.com>
2020-09-19 09:18:08 -0700

    Update all the things

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from '../components/Login';
+import Dashboard from '../components/Dashboard';
+import withAuthentication from '../containers/withAuthentication';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/dashboard" component={withAuthentication(Dashboard)} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
